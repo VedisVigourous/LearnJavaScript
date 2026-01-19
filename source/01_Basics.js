839504476f8c31bf824c0208a9e7fbb9e3772b48
VedisVigourous <vadanta592007@hotmail.com>
2026-01-19 18:03:47 +0530

    Printing in js

--- source/01_Basics.js
+++ source/01_Basics.js
@@ -1,2 +1,13 @@
 /* Owner - Vadanta Kumar Chauhaan : Github - VedisVigourous
    Date: 19th January 2026 */ 
+
+/* Note: To run javascript code we can use Node.js or any web browser's console (inspect mode)
+
+   Earlier, javascript was mainly executed on the browser console but with time Node.js was introduced which allowed us to run javascript code on a standalone environment outside the browser.
+
+    Node.js is a runtime environment that allows us to execute javascript code on the server-side or outside the browser. It uses the V8 JavaScript engine developed by Google, which is also used in Google Chrome. 
+    
+    Similar to Node.js we also have Deno.js both of which are founded by Ryan Dahl */
+
+// Printing a message to the console
+console.log("Hello, World!");
