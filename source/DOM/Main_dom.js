453261bf2db098009b063b2e6e8aaea6a20130a5
VedisVigourous <vadanta592007@hotmail.com>
2026-05-12 11:19:49 +0530

    Basic DOM Manipulation

--- source/DOM/Main_dom.js
+++ source/DOM/Main_dom.js
@@ -16,6 +16,12 @@ console.log(document.head)
 console.log(document.all) // Provides with all the used tags with indexing according to order used!
 console.log(document.all[10]) // Grabbing of a tag using it's index to get it's details and props
 
+// We can also change the content of a tag using it's index in the document.all array
+// document.all[14].textContent = "Hello World" // Changes the text of the tag with index 14 to "Hello World"
+
+// Note: If a tag is not present in the document and we console.log it, it will return null
+console.log(document.links) // null - because there are no links in the document, if there were any links then it would have returned an array of all the links present in the document
+
 
 /* Note: All these items are not read-only and can be changed from the script 
    - Ex. Changing title of webpage from script */
