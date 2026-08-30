35592f1ed76f325270a60f12816145cf49e5acd5
VedisVigourous <vadanta592007@hotmail.com>
2026-08-30 18:46:04 +0530

    Control Flow Conditionals

--- source/CH06_ControlFlow/23_ControlFlowIntro.js
+++ source/CH06_ControlFlow/23_ControlFlowIntro.js
@@ -64,7 +64,7 @@ else {
    - It is written in a single line.
    - Curly Braces are not required. */
 
-if (true) console.log("Implicit Condition True");
+if (true) console.log("> Implicit Condition True\n");
 
 /* If there are more than two conditions
    - We can use if-else if ladder.
@@ -73,13 +73,13 @@ if (true) console.log("Implicit Condition True");
 let money = 1000
 
 if (money < 500) {
-    console.log("Money less than 500");
+    console.log("> Money less than 500");
 }
 else if (money < 800){
-    console.log("Money less than 800");
+    console.log("> Money less than 800");
 }
 else {
-    console.log("Money greater than 800");
+    console.log("> Money greater than 800");
     
 }
 
@@ -93,16 +93,98 @@ const mobileLogin = false
 const emailLogin = true
 
 if (newUser && emailLogin) {
-    console.log("Good to go...");
+    console.log("> Good to go...");
 }
 else if (newUser && mobileLogin){
-    console.log("Update Email");
+    console.log("> Update Email");
 }
 else if (oldUser && (mobileLogin || emailLogin)){
-    console.log("Old User ... Good to go")
+    console.log("> Old User ... Good to go \n")
 }
 
 
 
 
 
+/* Switch statement - executes a block of code based on the matching case value
+   - It is used when there are multiple conditions to check.
+   - It is more efficient than if-else if ladder. 
+   
+   Syntax:
+   
+   switch (key) {
+    case value1:
+        // block of code
+        break;
+    case value2:
+        // block of code
+        break;
+    default:
+        // block of code
+   } 
+        
+   here, key is the variable whose value is compared against the case values. 
+   - Also, values to be compared can be of different data types. */
+
+let day = 2
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid Day");
+}
+
+
+
+/* NOTE: If break is not used after each case, the following code is automatically executed until a break is found or the switch statement ends.
+   - This is called as {fall through.} */
+
+
+
+/* Nullish Coalescing Operator (??) - It is used to provide a default value for a variable if it is null or undefined. 
+- It's working revolves around 'null' and 'undefined' values.
+
+- Imagine getting two values for a variable,
+  1. being null or undefined, and
+    2. being a valid value (like a number, string, etc.)
+
+    then the nullish coalescing operator (??) will return the valid value if the first value is null or undefined, otherwise it will return the first value. */
+
+let userName = null ?? "Vedant"
+
+/* Since it has two option either null or 'Vedant' it will return the valid value */
+
+console.log(`\n> Value of UserName - ${userName}`);
+
+
+
+/* Ternary Operator - It is a shorthand for if-else statement.
+   - It is used to assign a value to a variable based on a condition.
+   - It is also called as conditional operator. 
+   
+   Syntax:
+    condition ? trueValue : falseValue */
+
+const ranVar = 5 > 2 ? 5 : 2
+
+// Here, the condition is true, so the value of ranVar will be 5.
